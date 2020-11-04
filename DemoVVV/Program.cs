34dc014e94049fd0ed177ad406ce9fdc682c369c
Olesya Geds <OGED127@office.cc-student.com>
2020-11-04 23:20:38 +0100

    Methoden

--- DemoVVV/Program.cs
+++ DemoVVV/Program.cs
@@ -6,8 +6,36 @@ namespace DemoVVV
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! Hello Olesya");
-        
+            int MeineZahl = 12;
+
+            int PreRueckgabe = PreIncrement(ref MeineZahl);
+            int PostRueckgabe = PostIncrement(ref MeineZahl);
+
+        }
+
+        /// Erstellen von Methoden welche ++ nachstellen
+        /// 1. Name: PostIncrement
+        ///     Originalvariable sichern
+        ///     Original erhöhen
+        ///     Sicherung zurückgeben
+        static int PostIncrement(ref int pZahl)
+        {
+            int backup;
+            backup = pZahl;
+            // alternativ  int backup = pZahl;
+
+            pZahl = pZahl + 1; // alternativ  pZahl += 1;
+            return backup;
+        }
+
+
+        /// 2. Name: PreIncrement
+        ///     Original erhöhen
+        ///     Original zurückgeben
+        static int PreIncrement(ref int pZahl)
+        {
+            pZahl = pZahl + 1;
+            return pZahl;
         }
     }
 }
