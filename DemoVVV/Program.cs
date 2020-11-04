d503911166d1ac25ab35127806af108beb6aee06
Olesya Geds <OGED127@office.cc-student.com>
2020-11-04 21:09:09 +0100

    Add project files.

---
+++ DemoVVV/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DemoVVV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Hello Olesya");
+        
+        }
+    }
+}
